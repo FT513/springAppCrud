416540101bfa543fe235564b676f8e2a22b5dd25
FT513 <black.jack513@yahoo.com>
2024-02-08 19:17:27 +0100

    Initial commit

---
+++ springappcrud/src/main/java/com/springpractice/springappcrud/SpringAppCrudApplication.java
@@ -0,0 +1,14 @@
+package com.springpractice.springappcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppCrudApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringAppCrudApplication.class, args);
+	}
+
+}
